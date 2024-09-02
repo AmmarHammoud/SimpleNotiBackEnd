9dbb0d8c1b22dca4cd5d2e58081be3c881b87f6d
AliAlmskee <ali.mski2001@gmail.com>
2024-09-02 21:35:22 +0300

    test2

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
 
 Route::post('notiKeys', [MyController::class, "sendNotificationWithDifferentKeys"]);
+
+
+Route::get('/hello', function () {
+    return response()->json(['message' => 'HELLO']);
+});
